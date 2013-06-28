04c367001b3ebc8960fdeade8142cfc9a9793ca9
Bogdan Begovic <Bogdan Begovic@RS-DW-014.drey.rs>
2013-06-28 16:58:38 +0200

    Fixing the card flip!!!

--- src/main/webapp/js/testFlip.js
+++ src/main/webapp/js/testFlip.js
@@ -3,31 +3,40 @@
  * and open the template in the editor.
  */
 
-$(function() {
-    
-    var turnRotator = false;
-    
-    
-    
-    $("#btn-turn").on("click", function(e) {
-        
-        if (!turnRotator){
+var turnRotator = false;
+
+
+$("#btn-turn").on("click", function(e) {
+
+    var cardQuestion = $('.cardQuestionPlace').text();
+    var cardAnswer = $('.cardAnswerPlace').text();
+    if (!turnRotator) {
         $(".flipbox").flippy({
             color_target: "",
             direction: "top",
             duration: "750",
-            verso: "<span>Gore</span>"
+            verso: "<span class='cardTextSelectorQuestion'>" + cardQuestion + "</span>"
         });
         turnRotator = true;
-    }else{
+    } else {
         $(".flipbox").flippy({
             color_target: "",
             direction: "bottom",
             duration: "750",
-            verso: "<span>Dole</span>"
+            verso: "<span class='cardTextSelectorAnswer'>" + cardAnswer + "</span>"
         });
         turnRotator = false;
-    }          
-        e.preventDefault();
-    });
+    }
+    e.preventDefault();
+});
+
+$(".alt_btn_preview").on("click", function(e) {
+    alert("radi prew");
+        if (!turnRotator) {
+        $(".cardTextSelectorQuestion").text( $('.cardQuestionPlace').text());
+    } else {
+       $(".cardTextSelectorAnswer").text($('.cardAnswerPlace').text());
+    }
+
+   
 });
